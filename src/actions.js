769ac2b55e039caeb5a5b17b25b05b2d7cad3fa1
David Schmotz <davidschmotz@gmx.de>
2020-08-17 20:47:56 +0200

    added display and timer

--- src/actions.js
+++ src/actions.js
@@ -4,13 +4,32 @@
  * @Email:  davidschmotz@gmail.com
  * @Filename: actions.js
  * @Last modified by:   David
- * @Last modified time: 2020-08-17T20:37:35+02:00
+ * @Last modified time: 2020-08-17T20:47:31+02:00
  */
 
 const helper = require('./helper')
 
-const generateBut = document.getElementById("generate_but");
-generateBut.addEventListener("click", () => {
+const generateNumberBut = document.getElementById("generateNumberBut");
+generateNumberBut.addEventListener("click", () => {
+  let n = helper.generateRandomInt(2, 20);
+  let digits = helper.generateRandomInt(1,3);
+  var randomNumbers = [];
+  for (var i=0; i<=n; i++) {
+    var randomNumber = helper.generateRandomInt( 0 , Math.pow(10,digits) );
+    randomNumbers.push( randomNumber );
+  }
+
+  let outputLabel = document.getElementById("outputLabel")
+  outputLabel.innerHTML = randomNumbers.join("  ");
+
+  setInterval(function(){
+    outputLabel.innerHTML = "";
+  }, (n*700 + digits*1000) );
+
+});
+
+const generateSequenceBut = document.getElementById("generateSequenceBut");
+generateSequenceBut.addEventListener("click", () => {
   let n = helper.generateRandomInt(2, 20);
   let digits = helper.generateRandomInt(1,3);
   var randomNumbers = [];
