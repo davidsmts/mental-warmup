dfca84956256b00d4acf71d4197245df87df75ee
David Schmotz <davidschmotz@gmx.de>
2020-08-18 17:22:49 +0200

    - random sequence generation + display

--- src/actions.js
+++ src/actions.js
@@ -4,13 +4,14 @@
  * @Email:  davidschmotz@gmail.com
  * @Filename: actions.js
  * @Last modified by:   David
- * @Last modified time: 2020-08-17T20:47:31+02:00
+ * @Last modified time: 2020-08-18T17:21:53+02:00
  */
 
 const helper = require('./helper')
 
 const generateNumberBut = document.getElementById("generateNumberBut");
 generateNumberBut.addEventListener("click", () => {
+  //
   let n = helper.generateRandomInt(2, 20);
   let digits = helper.generateRandomInt(1,3);
   var randomNumbers = [];
@@ -18,25 +19,26 @@ generateNumberBut.addEventListener("click", () => {
     var randomNumber = helper.generateRandomInt( 0 , Math.pow(10,digits) );
     randomNumbers.push( randomNumber );
   }
-
+  //
   let outputLabel = document.getElementById("outputLabel")
   outputLabel.innerHTML = randomNumbers.join("  ");
-
+  //
   setInterval(function(){
     outputLabel.innerHTML = "";
   }, (n*700 + digits*1000) );
-
 });
 
 const generateSequenceBut = document.getElementById("generateSequenceBut");
 generateSequenceBut.addEventListener("click", () => {
+  // get random sequence
   let n = helper.generateRandomInt(2, 20);
-  let digits = helper.generateRandomInt(1,3);
-  var randomNumbers = [];
-  for (var i=0; i<=n; i++) {
-    var randomNumber = helper.generateRandomInt( 0 , Math.pow(10,digits) );
-    randomNumbers.push( randomNumber );
-  }
+  let randomSeq = helper.generateRandomSequence(n);
+  // display random sequence
+  let outputLabel = document.getElementById("outputLabel")
+  outputLabel.innerHTML = randomSeq
+  // hide random sequence
+  setInterval(function(){
+    outputLabel.innerHTML = "";
+  }, (n*1000) );
 
-  console.log(randomNumbers)
 });
--- src/helper.js
+++ src/helper.js
@@ -4,7 +4,7 @@
  * @Email:  davidschmotz@gmail.com
  * @Filename: helper.js
  * @Last modified by:   David
- * @Last modified time: 2020-08-17T20:33:45+02:00
+ * @Last modified time: 2020-08-18T17:21:15+02:00
  */
 
 
@@ -12,7 +12,17 @@ const generateRandomInt = (from, to) => {
   return from + Math.floor(Math.random() * to);
 }
 
+const generateRandomSequence = (len) => {
+  var result           = '';
+  var chars       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charsLength = chars.length;
+  for ( var i = 0; i < len; i++ ) {
+     result += chars.charAt(Math.floor(Math.random() * charsLength));
+  }
+  return result;
+}
 
 module.exports = {
-  generateRandomInt
+  generateRandomInt,
+  generateRandomSequence
 }
